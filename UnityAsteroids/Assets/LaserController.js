015f7aa6a99b6b3482df795e94fdc43753c84edd
unknown <grazbona@L037-08.mcast.org.mt>
2013-03-13 09:16:51 +0100

    moving the space ship and shooting laser

---
+++ UnityAsteroids/Assets/LaserController.js
@@ -0,0 +1,17 @@
+#pragma strict
+var laserspeed:int;
+
+function Start () {
+
+}
+
+function Update () {
+	//all the laser needs is forward motion
+	transform.Translate(Vector3.up * laserspeed * Time.deltaTime);
+}
+
+//when the laser exits the scene
+function OnBecameInvisible()
+{
+	Destroy(this.gameObject);
+}
